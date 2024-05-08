57834702b6b50d7ad3c4c57e3f21d06253562d8b
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-08 20:56:35 +0100

    Ex 4, add test code

---
+++ EqualPlayer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EqualPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test code
+            HashSet<Player> setOfPlayers = new HashSet<Player>();
+
+            foreach (Player p in setOfPlayers)
+            {
+                Console.WriteLine($"{p.Name} is a {p.PClass}");
+            }
+            
+        }
+    }
+}
