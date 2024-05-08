a9a0f6750f42eab272433ca882fd0eac83c61d8f
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-08 20:55:48 +0100

    Ex 4, add override GetHashCode and Equals methods, and comments

--- EqualPlayer/Player.cs
+++ EqualPlayer/Player.cs
@@ -14,5 +14,19 @@ namespace EqualPlayer
             PClass = pClass;
             Name = name;
         }
+
+        //GetHashCode method override so that a player is considered unique 
+        //in the game if it has the same type and the same name
+        public override int GetHashCode() => PClass.GetHashCode() ^ Name.GetHashCode()
+
+        //Equals method override so that a player is considered unique 
+        //in the game if it has the same type and the same name
+        public override bool Equals(object obj)
+        {
+            Player other = obj as Player;
+            if (other == null) return false;
+            return PClass == other.PClass && Name == other.Name;
+        }
+
     }
 }
