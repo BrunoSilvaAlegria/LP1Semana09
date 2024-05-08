c031469e6aef417657a261f5e32270a1665f9430
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-08 19:58:32 +0100

    Ex 1, correct collections display and add foreach cycle for each collection, functional and with comments

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -7,11 +7,54 @@ namespace IntCollections
     {
         static void Main(string[] args)
         {
+            int[] array = new int [5] { 1, 10, -30, 10, -5 }; // Numbers to test
+            
             // Different collections effect on the same group of numbers
-            List<int> list = new List<int> { 1, 10, -30, 10, -5 };
-            Stack<int> stack = new Stack<int> { 1, 10, -30, 10, -5 };
-            Queue<int> queue = new Queue<int> { 1, 10, -30, 10, -5 };
-            HashSet<int> hashSet = new HashSet<int> { 1, 10, -30, 10, -5 };
+            List<int> list = new List<int>();
+            Stack<int> stack = new Stack<int>();
+            Queue<int> queue = new Queue<int>();
+            HashSet<int> hashSet = new HashSet<int>();
+
+            // Cycle that adds each number from the array to each collections
+            foreach (int n in array) 
+            {
+                list.Add(n);
+                stack.Push(n);
+                queue.Enqueue(n);
+                hashSet.Add(n);
+            }
+
+            // For the List collection case
+            string str = "List: "; 
+            foreach (int n in list)
+            {
+                str += $"{n}, ";
+            }
+            Console.WriteLine(str.Remove(str.Length - 2));
+
+            // For the Stack collection case
+            str = "Stack: "; 
+            foreach (int n in stack)
+            {
+                str += $"{n}, ";
+            }
+            Console.WriteLine(str.Remove(str.Length - 2));
+
+            // For the Queue collection case
+            str = "Queue: "; 
+            foreach (int n in queue)
+            {
+                str += $"{n}, ";
+            }
+            Console.WriteLine(str.Remove(str.Length - 2));
+
+            // For the HashSet collection case
+            str = "HashSet: "; 
+            foreach (int n in hashSet)
+            {
+                str += $"{n}, ";
+            }
+            Console.WriteLine(str.Remove(str.Length - 2));
         }
     }
 }
