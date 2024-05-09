d477f989e97c1257ca6a62cc05b4d8855308795f
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-09 14:32:11 +0100

    Ex 6, copy Program from FilePower1

---
+++ FilePower2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = args[0]; //Gets the argument
+            
+            if (args.Length <= 0) //Verifies if the argument was entered
+            {
+                Console.WriteLine("Please enter a file name.");
+                return;
+            }
+
+            if (!File.Exists(fileName)) //Verifies if the file exists
+            {
+                Console.WriteLine("This file does not exist, " +
+                "please enter a valid file name.");
+                return;
+            }
+
+            //Queue to store future written strings
+            Queue<string> strs = new Queue<string>();
+
+            string fileLines; //Variable that stores the written strings
+
+            while(true)
+            {
+                //Reads and stores the written strings
+                fileLines = Console.ReadLine();
+
+                if (fileLines.Length <= 0) break; //If the string is empty, exits
+
+                strs.Enqueue(fileLines); //Adds the written strings to the queue
+            }
+
+            //Puts the strings stored into the original file
+            File.WriteAllLines(fileName, strs); 
+        }
+    }
+}
