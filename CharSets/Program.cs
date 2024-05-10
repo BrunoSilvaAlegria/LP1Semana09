a81beb395ff85e7bbd99ea44cf2d16d6fba202b5
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-10 15:13:57 +0100

    Ex extra, add characters storage and addition, intersect arguments test

--- CharSets/Program.cs
+++ CharSets/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 
-namespace FilePower2
+namespace CharSets
 {
     class Program
     {
@@ -12,28 +12,39 @@ namespace FilePower2
             {
                 //Gets the argument correspondent to a position
                 string fileName = args[n];
+
+                //Characters storage
+                HashSet<char> chars = new HashSet<char>();
+
                 if (File.Exists(fileName)) //Checks if the file exists
                 {
                     //Opens the file for reading
                     using StreamReader strs = new StreamReader(fileName);
 
                     //Determines the end of the file or an error
-                    while (strs.Peek() > -1) 
+                    while (strs.Peek() > -1)
                     {
                         //Reads the file line by line
                         string line = strs.ReadLine();
                         if (line.Length > 1)
                         {
+                            //Error if there is more than 1 character in a line
                             Console.WriteLine("Error!");
                             break;
                         }
                         else
                         {
-                            Console.WriteLine(line);
+                            foreach (char c in line)
+                            {
+                                chars.Add(c); //Character addition to storage
+                                chars.IntersectWith(args[n + 1]);
+                            }
                         }
-                    } 
-                } 
+                    }
+
+                } //File is closed automatically due to the keyword "using"
+                else Console.WriteLine($"File {fileName} does not exist");
             }        
-        } //File is closed automatically due to the keyword "using" being used
+        } 
     }
 }
